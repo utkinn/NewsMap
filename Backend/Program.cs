daa5cb933f06fbe02f44a5137df79122067e0835
javabird25 <shockck84@gmail.com>
2023-02-28 13:00:25 +0500

    First commit

---
+++ Backend/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run("http://0.0.0.0:5000");
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [weatherForecast, setWeatherForecast] = useState<string>("Loading...");
+
+  useEffect(() => {
+    fetch("/api/WeatherForecast").then(resp => {
+      if (resp.ok) {
+        resp.text().then(setWeatherForecast);
+      } else {
+        resp.text().then(t => setWeatherForecast(`Ошибка ${resp.status}: ${t}`));
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          {weatherForecast}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
