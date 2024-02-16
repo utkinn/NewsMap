8551dcf7620f858f40dc3fb28da39ecc2a7bcdb7
Nikita Utkin <utkin@mindbox.cloud>
2024-02-16 19:00:26 +0500

    Add /api/articles and "fix" /api/articles/for-list

--- Backend/Controllers/News/ArticleController.cs
+++ Backend/Controllers/News/ArticleController.cs
@@ -12,13 +12,20 @@ public class ArticleController(
 	ArticleRepository articleRepository,
 	PostArticleRequestToModelConverter articleModelConverter)
 {
+	[HttpGet("/")]
+	public IEnumerable<ArticleResponse> Get() =>
+		articleRepository
+			.GetRelevantAtGivenDay(DateOnly.FromDateTime(DateTime.Now))
+			.Select(a => new ArticleResponse(a));
+
 	[HttpGet("/for-list")]
 	public IEnumerable<ArticleResponse> GetForList(
 		[FromQuery, BindRequired, RegularExpression(@"^\d{4}-\d{2}-\d{2}$")]
 		string date) =>
-		articleRepository.GetRelevantAtGivenDay(DateOnly.Parse(date)).Select(a => new ArticleResponse(a));
+		articleRepository.GetPublishedAtGivenDay(DateOnly.Parse(date)).Select(a => new ArticleResponse(a));
 
 	[HttpPost("/")]
+	// TODO: Authorize
 	public async Task Add([FromBody] PostArticleRequest article) =>
 		await articleRepository.AddAsync(await articleModelConverter.ToModelAsync(article));
 }
--- Backend/Repositories/News/ArticleRepository.cs
+++ Backend/Repositories/News/ArticleRepository.cs
@@ -5,18 +5,28 @@ namespace NewsMap.Repositories.News;
 
 public class ArticleRepository(NewsMapDbContext dbContext)
 {
-    public IEnumerable<Article> GetRelevantAtGivenDay(DateOnly date)
-    {
-        var todayBegin = new DateTimeOffset(date, new TimeOnly(0, 0), TimeSpan.Zero);
-        var todayEnd = new DateTimeOffset(date, new TimeOnly(23, 59), TimeSpan.Zero);
-        return dbContext.Articles
-            .Include(a => a.Tags)
-            .Where(a => todayBegin <= a.RelevantTo && a.RelevantFrom <= todayEnd);
-    }
+	public IEnumerable<Article> GetRelevantAtGivenDay(DateOnly date)
+	{
+		var todayBegin = new DateTimeOffset(date, new TimeOnly(0, 0), TimeSpan.Zero);
+		var todayEnd = new DateTimeOffset(date, new TimeOnly(23, 59), TimeSpan.Zero);
+		return dbContext.Articles
+			.Include(a => a.Tags)
+			.Where(a => a.RelevantTo >= todayBegin && todayEnd >= a.RelevantFrom);
+		// TODO: check Where clause for correctness
+	}
 
-    public async Task AddAsync(Article article)
-    {
-        await dbContext.AddAsync(article);
-        await dbContext.SaveChangesAsync();
-    }
+	public IEnumerable<Article> GetPublishedAtGivenDay(DateOnly date)
+	{
+		var todayBegin = new DateTimeOffset(date, new TimeOnly(0, 0), TimeSpan.Zero);
+		var todayEnd = new DateTimeOffset(date, new TimeOnly(23, 59), TimeSpan.Zero);
+		return dbContext.Articles
+			.Include(a => a.Tags)
+			.Where(a => todayBegin <= a.PublishedAt && a.PublishedAt <= todayEnd);
+	}
+
+	public async Task AddAsync(Article article)
+	{
+		await dbContext.AddAsync(article);
+		await dbContext.SaveChangesAsync();
+	}
 }
