8a94260ba6ef7c5c71b1357961238d52e7aeaa56
Nikita Utkin <utkin@mindbox.cloud>
2024-05-20 18:00:11 +0500

    Use process.env.REACT_APP_API_BASE_URL in httpClient.ts

--- Frontend/src/httpClient.ts
+++ Frontend/src/httpClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: process.env.REACT_APP_API_BASE_URL
 });
 
 httpClient.interceptors.request.use(config => {
