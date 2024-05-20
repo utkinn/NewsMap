35c33b26d4ba6790410cc41eec65dce1e544fe2a
Nikita Utkin <utkin@mindbox.cloud>
2024-05-20 17:36:27 +0500

    Add Axios HTTP client with auth interceptor

---
+++ Frontend/src/httpClient.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const httpClient = axios.create({
+    baseURL: 'http://localhost:5000'
+});
+
+httpClient.interceptors.request.use(config => {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+        config.headers.Authorization = `Bearer ${jwt}`;
+    }
+    return config;
+});
