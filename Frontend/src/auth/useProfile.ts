a1070d3dc4b3d91e6d9ee62453ec8916aa3bb501
Nikita Utkin <utkin@mindbox.cloud>
2024-05-20 18:32:10 +0500

    Use vanilla localStorage in useProfile

--- Frontend/src/auth/useProfile.ts
+++ Frontend/src/auth/useProfile.ts
@@ -4,23 +4,22 @@ import {httpClient} from "../httpClient";
 import {Profile} from "./Profile";
 
 export function useProfile(): [Profile | null, boolean] {
-    const [jwt,] = useLocalStorage("jwt", null);
-
-    if (!jwt) {
-        return [null, true];
-    }
-
+    const jwt = localStorage.getItem("jwt");
     const [profile, setProfile] = useState<Profile | null>(null);
     const [ready, setReady] = useState(false);
 
     useEffect(() => {
+        if (!jwt) {
+            setReady(true);
+            return;
+        }
         httpClient
             .get<Profile>("/Profile")
             .then(p => {
                 setProfile(p.data);
                 setReady(true);
             })
-    }, []);
-    
+    }, [jwt]);
+
     return [profile, ready];
 }
