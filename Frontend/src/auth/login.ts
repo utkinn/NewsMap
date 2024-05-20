ea08bb7fb42dfed46ae8d06622d061fb8b84028c
Nikita Utkin <utkin@mindbox.cloud>
2024-05-20 18:29:41 +0500

    Fix login function

--- Frontend/src/auth/login.ts
+++ Frontend/src/auth/login.ts
@@ -1,5 +1,6 @@
 import {httpClient} from "../httpClient";
 
 export async function login(email: string, password: string) {
-    await httpClient.post("/Auth/login", {email, password});
+    const jwt = await httpClient.post<string>("/Auth/login", {email, password});
+    localStorage.setItem("jwt", jwt.data);
 }
