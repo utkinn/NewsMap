74e1ec6fb2639bbec6bf7cfba6b17d45aa0d88ed
maxfo <max.fom.vic@gmail.com>
2024-06-20 19:35:32 +0500

    last

--- Frontend/src/components/userPanel/userPanel.tsx
+++ Frontend/src/components/userPanel/userPanel.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useAppSelector } from "../../app/hooks";
+import {ReactComponent as EmptyUserIcon} from "../../icons/emptyUser.svg";
 
 export const UserPanel = () => {
     const logged = useAppSelector(state => state.user.logged);
     return (
         <div>
-            
+            {!logged ? <React.Fragment> <EmptyUserIcon/> <button>Войдите</button> </React.Fragment> : <div></div>}
         </div>
     );
 }
