6888c03051978a3207da6137fc519dc219c0f5da
Nikita Utkin <shockck84@gmail.com>
2024-06-20 20:10:42 +0500

    Remove junk

--- Frontend/src/auth/login.ts
+++
@@ -1,6 +0,0 @@
-import {httpClient} from "../httpClient";
-
-export async function login(email: string, password: string) {
-    const jwt = await httpClient.post<string>("/Auth/login", {email, password});
-    localStorage.setItem("jwt", jwt.data);
-}
